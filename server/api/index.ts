579d795db43ffe681175be5903e03a25e56a4ae7
ShirefMohammed <shirefmo012345@gmail.com>
2025-02-04 16:37:12 +0200

    Commit for deployment

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,3 +1,35 @@
-import app from "../src/server";
+// import app from "../src/server";
+// export default app;
+import app, { initializeApp } from "../src/app";
+import { disconnectDB } from "../src/database";
 
-export default app;
+(async () => {
+  await initializeApp();
+
+  const _PORT = process.env.PORT;
+
+  // Gracefully shut down the server and close database connections
+  process.on("SIGINT", async () => {
+    console.log("Server is shutting down...");
+    await disconnectDB(); // Close database connections
+    process.exit(0); // Exit with success code
+  });
+
+  process.on("SIGTERM", async () => {
+    console.log("Received termination signal...");
+    await disconnectDB(); // Close database connections
+    process.exit(0); // Exit with success code
+  });
+
+  // Start the server
+  app.listen(_PORT, () => {
+    console.log(
+      "\x1b[32m%s\x1b[0m",
+      `Server running on ${process.env.SERVER_URL}`,
+    );
+    console.log(
+      "\x1b[36m%s\x1b[0m",
+      `Swagger running on ${process.env.SERVER_URL}/api-docs`,
+    );
+  });
+})();
