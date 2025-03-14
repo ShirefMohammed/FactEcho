d6bbe3f326b7ddcd58eb282016b86ad918ae66f6
ShirefMohammed <shirefmo012345@gmail.com>
2025-03-14 21:25:37 +0200

    Updating rateLimiter

--- server/src/middleware/limiter.ts
+++ server/src/middleware/limiter.ts
@@ -4,14 +4,14 @@ import slowDown from "express-slow-down";
 // Rate limiting
 export const rateLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 minutes
-  max: 30, // Limit each IP to 30 requests per windowMs
+  max: 60, // Limit each IP to 60 requests per windowMs
   message: "Too many requests from this IP, please try again later.",
 });
 
 // Slow down repeated requests
 export const speedLimiter = slowDown({
   windowMs: 60 * 1000, // 1 minutes
-  delayAfter: 30, // Allow 30 requests per windowMs before slowing down
+  delayAfter: 60, // Allow 60 requests per windowMs before slowing down
   delayMs: (used, req) => {
     // Calculate delay dynamically based on the number of requests
     const delayAfter = req.slowDown.limit;
